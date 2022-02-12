00d61c35b15af1701f89a41b3ec18c860d8488a5
EddieJorden <eddie@eddiejorden.tech>
2022-02-12 15:58:21 -0800

    added my projects button and page with link to food.near.me

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import MyResume from './components/myResume/MyResume';
 import Home from './components/home/Home';
 import ThreeJs from './components/threeJsBackground/ThreeJsRoute';
+import AboutMe from './components/aboutMe/AboutMe';
+import MyProjects from './components/myProjects/MyProjects';
 
 function App() {
 
@@ -15,6 +17,8 @@ function App() {
           <Route path="/ContactInfo" element={<ContactInfo />}/>
           <Route path="/Resume" element={<MyResume />}/>
           <Route path="/threejs" element={<ThreeJs />}/>
+          <Route path="/AboutMe" element={<AboutMe />}/>
+          <Route path="/MyProjects" element={<MyProjects />}/>
         </Routes>
       </div>
     </div>
---
+++ src/components/myProjects/MyProjects.jsx
@@ -0,0 +1,10 @@
+const MyProjects = () => {
+    return (
+        <div>
+            <h1>food.near.me</h1>
+            <a href="https://eddiejorden.github.io/food.near.me/" rel="noopener noreferer">food.near.me</a>
+        </div>
+    )
+};
+
+export default MyProjects;
