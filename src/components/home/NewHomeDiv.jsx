966551b508df9d42c410d7598bc5e4e895219ddb
EddieJorden <eddie@eddiejorden.tech>
2022-03-24 06:51:42 -0700

    fixed responsiveness

--- src/components/home/NewHomeDiv.jsx
+++ src/components/home/NewHomeDiv.jsx
@@ -6,31 +6,6 @@ const NewHomeDiv = styled.div`
     display: flex;
     align-items: center;
 
-    .content-div {
-        margin-top: 30px;
-        margin-right: 30px;
-        color: white;
-        width: 61.8vw;
-        max-height: 100%;
-        max-width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
-
-    .content-window {
-        max-width: 80vw;
-        max-height: 80vh;
-        background-color: white;
-        color: black;
-        overflow: scroll;
-        border-radius: 5px;
-        display: flex;
-        flex-direction: column;
-        text-align: -webkit-center;
-    }
-
     @media only screen and (max-width: 900px) {
         flex-direction: column-reverse;
     }
