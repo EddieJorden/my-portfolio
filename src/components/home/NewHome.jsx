5bdf991b1d59ffd23194259f377fe8790c7dc0cf
EddieJorden <eddie@eddiejorden.tech>
2022-04-03 17:32:15 -0700

    made nicer buttons

--- src/components/home/NewHome.jsx
+++ src/components/home/NewHome.jsx
@@ -14,10 +14,11 @@ import ProjectsRenderer from './ProjectsRenderer';
 import ComponentDiv from '../componentDiv/ComponentDiv';
 
 const NewHomeButton = styled.button`
-    background: transparent;
+    background-color: #1a77f2;
     border-radius: 6px;
     border: 2px solid #1a77f2;
-    color: #1a77f2;
+    color: #ffffff;
+    font-weight: bold;
     padding: 0.5vw 1.5vw;
     margin: 4px;
     transition: 375ms;
@@ -25,7 +26,7 @@ const NewHomeButton = styled.button`
     :hover {
         cursor: pointer;
         border: 4px solid #1a77f2;
-        color: #1a77f2;
+        color: #ffffff;
         font-weight: bold;
         margin: 2px;
         transition: 375ms
