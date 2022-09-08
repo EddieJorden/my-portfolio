55e7c22c34c4f1641e20b59354420c30832de110
EddieJorden <eddie@eddiejorden.tech>
2022-09-08 13:04:13 -0700

    updated thumbs and links

--- src/components/myResume/MyResume.jsx
+++ src/components/myResume/MyResume.jsx
@@ -3,18 +3,28 @@ import { MyResumeDiv } from './styled_myResume';
 import styled from 'styled-components';
 
 const ResumeDiv = styled.div`
-    img {
-        max-height: 100%;
-    }
+	img {
+		max-height: 100%;
+	}
 `;
 
 const MyResume = () => {
 	return (
 		<MyResumeDiv>
 			<ResumeDiv>
-				<img src="Eddie Moger Software Engineer Resume page 1.png" alt="my-resume-thumbnail"/>
-				<img src="Eddie Moger Software Engineer Resume page 2.png" alt="my-resume-thumbnail"/>
-				<p>click <span><a href="Eddie Moger Software Engineer Resume.pdf" target="blank">here</a>{' '}</span>to view and download the pdf.</p>
+				<img src="Resume_p1_thumb.png" alt="my-resume-thumbnail" />
+				<img src="Resume_p2_thumb.png" alt="my-resume-thumbnail" />
+				<p>
+					click{' '}
+					<span>
+						<a
+							href="SEP_2022_Software_Engineer_Resume.pdf"
+							target="blank">
+							here
+						</a>{' '}
+					</span>
+					to view and download the pdf.
+				</p>
 			</ResumeDiv>
 		</MyResumeDiv>
 	);
