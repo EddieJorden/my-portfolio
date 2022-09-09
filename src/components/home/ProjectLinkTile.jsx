178656a2d735204533dd751d2dfa7fa7a408dae3
EddieJorden <eddie@eddiejorden.tech>
2022-09-09 10:49:08 -0700

    fixed projects list links

--- src/components/home/ProjectLinkTile.jsx
+++ src/components/home/ProjectLinkTile.jsx
@@ -4,32 +4,51 @@
 import styled from 'styled-components';
 
 const ProjectLinkTileDiv = styled.div`
-    display: flex;
+	display: flex;
 
-    min-width: 100%;
-    margin-left: 0px;
-    text-align: left;
+	min-width: 100%;
+	margin-left: 0px;
+	text-align: left;
 
-    color: white;
-    margin: 7px 0px;
-    border-radius: 10px;
-    padding: 5px;
+	color: white;
+	margin: 7px 0px;
+	border-radius: 10px;
+	padding: 5px;
 
-    img {
-        max-width: 78px;
-        margin: 5px;
-    }
+	img {
+		max-width: 78px;
+		margin: 5px;
+	}
 `;
 
 const ProjectLinkTile = ({ project }) => {
+	console.log(project);
 	return (
 		<ProjectLinkTileDiv>
-			<a href={project.link} target="blank" noopener="true" noreferer="true">
-				<div><img src={project.thumbnail} alt={project.name}/></div>
+			<a
+				href={project.link}
+				target="blank"
+				noopener="true"
+				noreferer="true">
+				<div>
+					<img src={project.thumbnail} alt={project.name} />
+				</div>
 			</a>
-			<div style={{margin: '5px'}}>
-				<a href='{project.github}' style={{textDecoration: 'none', color: '#1a77f2', fontWeight: 'bold'}}><div>{project.name}</div></a>
-				<a href='{project.github}' ><div><div>github</div></div></a>
+			<div style={{ margin: '5px' }}>
+				<a
+					href="{project.github}"
+					style={{
+						textDecoration: 'none',
+						color: '#1a77f2',
+						fontWeight: 'bold',
+					}}>
+					<div>{project.name}</div>
+				</a>
+				<a href={project.github}>
+					<div>
+						<div>github</div>
+					</div>
+				</a>
 			</div>
 		</ProjectLinkTileDiv>
 	);
