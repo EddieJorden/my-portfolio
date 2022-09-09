637d2774d7f0ef92ceb1ad897317a72512e121f4
EddieJorden <eddie@eddiejorden.tech>
2022-09-09 06:41:04 -0700

    changed meta og:image link

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,30 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 
 const HomeDiv = styled.div`
-    max-height: 100%;
-    overflow: none;
-    object-fit: contain;
+	max-height: 100%;
+	overflow: none;
+	object-fit: contain;
 `;
 
 const ImageDiv = styled.div`
-    max-hegiht: 100%;
- max-width: 100%;
-    object-fit: contain;
-    overflow: none;
-    
-    img {
-        object-fit: contain;
-        overflow: hidden;
-        width: 500px
-    }
-`;
+	max-hegiht: 100%;
+	max-width: 100%;
+	object-fit: contain;
+	overflow: none;
 
+	img {
+		object-fit: contain;
+		overflow: hidden;
+		width: 500px;
+	}
+`;
 
 const Home = () => {
 	return (
 		<HomeDiv>
 			<ImageDiv>
-				<img src="EddieInTheCity.jpg" alt="Eddie in the city"/>
+				<img src="EddieInTheCity.jpg" alt="Eddie in the city" />
 			</ImageDiv>
 		</HomeDiv>
 	);
