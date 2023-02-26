0e6253c4c75c8cf8f3788edbb5ee2fbef80729a8
EddieJorden <eddie@eddiejorden.tech>
2023-02-26 09:10:24 -0800

    updated styling for certifications, added box shadow to all images app wide, add hover effect to images wrapped in <a> tag app wide

--- src/components/certifications/Certifications.jsx
+++ src/components/certifications/Certifications.jsx
@@ -1,39 +1,49 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 import {ImageDiv, TextDiv, AboutMeDiv, ImageDescription} from '../../utils';
+import styled from 'styled-components';
+
+const CertificationDiv = styled.div`
+	background-color: #f1f1f1;
+	border: 1px solid #d3d3d3;
+	border-radius: 5px;
+	padding: 10px;
+	margin: 10px;
+	text-align: center;
+`;
 
 const Certifications = () => {
 	return (
 		<AboutMeDiv>
 			<ImageDiv>
-				<a href={'https://www.codecademy.com/profiles/eddieJorden.tech'} target="_blank" rel="noopener noreferrer" title="My profile on Codecademy.com" aria-label="Codecademy profile and certificates"><img src={'Eddie-Moger-Front-End-Web-Development-Certificate.png'}/></a>
+				<a href={'https://www.codecademy.com/profiles/eddieJorden.tech'} target="_blank" rel="noopener noreferrer" title="My profile on Codecademy.com" aria-label="Codecademy profile and certificates" ><img src={'Eddie-Moger-Front-End-Web-Development-Certificate.png'}/></a>
 				<ImageDescription>Front End Web Development certificate of completion with Codecademy.com</ImageDescription>
 			</ImageDiv>
 			<TextDiv>I am a web development professional with a passion for software development. I have completed numerous courses and have obtained numerous certifications from Codecademy.com in various technologies and disciplines such as React, Redux, Typescript and much more. These certifications showcase my commitment and dedication to the field of web development. My enthusiasm for the subject is evident in the fact that I am continuously seeking to expand my knowledge, and I am currently exploring the back-end aspect of web development and machine learning. I am confident in my ability to bring my passion, excitement, and expertise to any project.</TextDiv>
 			<h3 >Certifications inlcude</h3>
 			<div style={{display: 'flex', flexWrap: 'wrap'}}>
-				<ul>Learn HTML</ul>
-				<ul>Learn CSS</ul>
-				<ul>Learn JavaScript</ul>
-				<ul>Learn TypeScript</ul>
-				<ul>Learn Node.js</ul>
-				<ul>Learn CSS-in-JS</ul>
-				<ul>Learn Redux</ul>
-				<ul>Learn React</ul>
-				<ul>Learn Intermediate JavaScript</ul>
-				<ul>Learn JavaScript: Error Handling</ul>
-				<ul>Learn JavaScript: Asynchronous Programming</ul>
-				<ul>Learn JavaScript Unit Testing</ul>
-				<ul>Front-End Engineer</ul>
-				<ul>Create a Front-End App with React</ul>
-				<ul>Create an Advanced Web App with React and Redux
-				</ul>
-				<ul>Build a Website with HTML, CSS, and GitHub Pages
-				</ul>
-				<ul>Learn Navigation Design</ul>
-				<ul>Learn Color Design</ul>
-				<ul>Learn CSS: Responsive Design</ul>
-				<ul>Learn Git & GitHub</ul>
+				<CertificationDiv>Learn HTML</CertificationDiv>
+				<CertificationDiv>Learn CSS</CertificationDiv>
+				<CertificationDiv>Learn JavaScript</CertificationDiv>
+				<CertificationDiv>Learn TypeScript</CertificationDiv>
+				<CertificationDiv>Learn Node.js</CertificationDiv>
+				<CertificationDiv>Learn CSS-in-JS</CertificationDiv>
+				<CertificationDiv>Learn Redux</CertificationDiv>
+				<CertificationDiv>Learn React</CertificationDiv>
+				<CertificationDiv>Learn Intermediate JavaScript</CertificationDiv>
+				<CertificationDiv>Learn JavaScript: Error Handling</CertificationDiv>
+				<CertificationDiv>Learn JavaScript: Asynchronous Programming</CertificationDiv>
+				<CertificationDiv>Learn JavaScript Unit Testing</CertificationDiv>
+				<CertificationDiv>Front-End Engineer</CertificationDiv>
+				<CertificationDiv>Create a Front-End App with React</CertificationDiv>
+				<CertificationDiv>Create an Advanced Web App with React and Redux
+				</CertificationDiv>
+				<CertificationDiv>Build a Website with HTML, CSS, and GitHub Pages
+				</CertificationDiv>
+				<CertificationDiv>Learn Navigation Design</CertificationDiv>
+				<CertificationDiv>Learn Color Design</CertificationDiv>
+				<CertificationDiv>Learn CSS: Responsive Design</CertificationDiv>
+				<CertificationDiv>Learn Git & GitHub</CertificationDiv>
 			</div>
 			<a href={'https://www.codecademy.com/profiles/eddieJorden.tech'} target="_blank" rel="noopener noreferrer" title="My profile on Codecademy.com" aria-label="Codecademy profile and certificates">Codecademy Certifications</a>
 		</AboutMeDiv>
