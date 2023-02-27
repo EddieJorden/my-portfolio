14f4dd401163811bb2094758709bdcc2a3fc803b
EddieJorden <eddie@eddiejorden.tech>
2023-02-27 08:59:06 -0800

    created services componenet

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/react-in-jsx-scope */
+const Services = () => {
+	return (
+		<div className="services">
+			<h1>Services</h1>
+			<h2>Web Development</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+				voluptates, quod, quia, quae voluptatibus quidem quas voluptate
+				repudiandae quibusdam quos voluptatem. Quisquam, quae. Quisquam, quae.
+				Quisquam, quae. Quisquam, quae.
+			</p>
+			<h2>SEO</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+				voluptates, quod, quia, quae voluptatibus quidem quas voluptate
+				repudiandae quibusdam quos voluptatem. Quisquam, quae. Quisquam, quae.
+				Quisquam, quae. Quisquam, quae.
+			</p>
+			<h2>Consulting</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+				voluptates, quod, quia, quae voluptatibus quidem quas voluptate
+				repudiandae quibusdam quos voluptatem. Quisquam, quae. Quisquam, quae.
+				Quisquam, quae. Quisquam, quae.
+			</p>
+		</div>
+	);
+};
+
+export default Services;
