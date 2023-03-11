131016dc3d92c7f6e6a23e004d29ddaac80b7c07
EddieJorden <eddie@eddiejorden.tech>
2023-03-10 16:46:13 -0800

    slight style upgrade to services section

--- src/components/home/NewHomeDiv.jsx
+++ src/components/home/NewHomeDiv.jsx
@@ -3,7 +3,7 @@ import styled from  'styled-components';
 const NewHomeDiv = styled.div`
 		max-height: 100vh;
     max-width: 100vw;
-		height: 94vh;
+		height: 93vh;
     display: flex;
 		justify-content: space-between;
     align-items: center;
