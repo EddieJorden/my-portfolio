541240d7e262866b0ff351790a5e364859d39e6c
Arlo <Arlo.E.Dev@gmail.com>
2026-02-16 07:20:47 -0800

    Fix imports

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { motion } from 'framer-motion';
 import styled, { keyframes } from 'styled-components';
 import { useNavigate } from 'react-router-dom';
